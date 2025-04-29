d4241eaaa6d40c93b6571a4380e34c57675e5a68
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-04-29 17:54:30 +0300

    switch to light theme, update header and footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,6 +2,6 @@
   import { Footer, FooterCopyright } from "flowbite-svelte";
 </script>
 
-<Footer footerType="logo" class="rounded-none mt-auto">
+<Footer footerType="logo" class="rounded-none mt-auto bg-slate-800">
   <FooterCopyright href="/" by="Anton Fesenko" year={2025} />
 </Footer>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -31,21 +31,22 @@
   };
 </script>
 
-<Navbar fluid class="flex items-center justify-between flex-wrap px-4">
-  <!-- Logo -->
+<Navbar
+  fluid
+  class="flex items-center justify-between flex-wrap px-4 bg-slate-800"
+>
   <div class="flex items-center min-w-0">
     <NavBrand href="/" class="flex items-center">
       <img src="/driveline.png" class="me-3 h-6 sm:h-9" alt="DriveLine Logo" />
     </NavBrand>
   </div>
 
-  <!-- Search -->
   <div class="flex-1 flex justify-center px-2 min-w-0">
     <div class="relative w-full max-w-md">
       <div
         class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
       >
-        <SearchOutline class="w-4 h-4 text-gray-500 dark:text-gray-400" />
+        <SearchOutline class="w-4 h-4 text-gray-400" />
       </div>
       <Input
         id="search-navbar"
@@ -57,36 +58,36 @@
     </div>
   </div>
 
-  <!-- Icons -->
   <div class="flex items-center justify-end space-x-2 min-w-0">
-    <!-- Mobile Cart Icon  -->
     <Button
       color="none"
       data-collapse-toggle="mobile-menu-3"
       aria-controls="mobile-menu-3"
       aria-expanded="false"
-      class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+      class="md:hidden header-button p-2.5"
+      href="/shoppingCart"
     >
       <CartOutline size="lg" />
     </Button>
 
-    <NavHamburger />
+    <NavHamburger class="header-button hover:bg-slate-800" />
   </div>
+
   <NavUl class="items-center space-x-3">
     <NavLi href="/shoppingCart" class="flex items-center">
-      <CartOutline size="lg" />
+      <CartOutline size="lg" class="hidden md:block header-button" />
       <span class="block md:hidden ps-2">Cart</span>
     </NavLi>
     <NavLi href="/" class="flex items-center">
-      <InfoCircleOutline size="lg" />
+      <InfoCircleOutline size="lg" class="hidden md:block header-button" />
       <span class="block md:hidden ps-2">About</span>
     </NavLi>
     <NavLi href="/user/myorders" class="flex items-center">
-      <RectangleListSolid size="lg" />
+      <RectangleListSolid size="lg" class="hidden md:block header-button" />
       <span class="block md:hidden ps-2">My orders</span></NavLi
     >
     <NavLi href="/" class="flex items-center">
-      <UserCircleSolid size="lg" />
+      <UserCircleSolid size="lg" class="hidden md:block header-button" />
       <span class="block md:hidden ps-2">Profile</span></NavLi
     >
   </NavUl>
