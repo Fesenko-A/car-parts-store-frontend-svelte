7c2c8f540a35ce298b89dbd183091347ecb90129
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-05-08 10:52:51 +0300

    product/id page interaction with API - update wishlist button: add tooltip and make button disabled

--- src/routes/product/[id]/+page.svelte
+++ src/routes/product/[id]/+page.svelte
@@ -1,81 +1,98 @@
 <script lang="ts">
-  import { Badge, Breadcrumb, BreadcrumbItem, Button } from "flowbite-svelte";
+  import { page } from "$app/stores";
+  import {
+    Badge,
+    Breadcrumb,
+    BreadcrumbItem,
+    Button,
+    Tooltip,
+  } from "flowbite-svelte";
   import type { Product } from "../../../types";
+  import { onMount } from "svelte";
+  import { apiFetch } from "../../../api";
+  import { formatCurrency } from "../../../utils/utils";
 
-  export let data: {
-    product: Product;
-  };
+  let product: Product | null = null;
+
+  let productId: string;
+  $: productId = $page.params.id;
+
+  onMount(async () => {
+    product = await apiFetch(`/products/get/${productId}`);
+  });
 </script>
 
 <svelte:head>
-  <title>{data.product.name}</title>
+  <title>{product ? product.name : "Loading..."}</title>
 </svelte:head>
 
-{#if data.product}
+{#if product}
   <div class="flex flex-col lg:flex-row gap-8 p-8 max-w-7xl mx-auto">
     <div class="lg:w-2/5 w-full flex justify-center items-start">
       <img
-        src={data.product.imageUrl}
-        alt={data.product.name}
+        src={product.imageUrl}
+        alt={product.name}
         class="w-full max-w-md object-contain rounded-lg shadow-lg"
       />
     </div>
 
     <div class="lg:w-3/5 w-full space-y-6 py-4">
       <Breadcrumb class="text-lg">
-        <BreadcrumbItem>{data.product.category.name}</BreadcrumbItem>
-        <BreadcrumbItem>{data.product.brand.name}</BreadcrumbItem>
+        <BreadcrumbItem>{product.category.name}</BreadcrumbItem>
+        <BreadcrumbItem>{product.brand.name}</BreadcrumbItem>
       </Breadcrumb>
 
       <h1 class="text-2xl xl:text-3xl font-bold tracking-tight text-gray-900">
-        {data.product.name}
+        {product.name}
       </h1>
 
-      {#if data.product.specialTag}
+      {#if product.specialTag}
         <div class="my-2">
           <Badge large color="red" class="text-lg"
-            >{data.product.specialTag.name}</Badge
+            >{product.specialTag.name}</Badge
           >
         </div>
       {/if}
 
       <div class="flex items-center gap-3 my-6">
-        {#if data.product.discountPercentage > 0}
+        {#if product.discountPercentage > 0}
           <span class="text-red-600 font-bold text-4xl">
-            ${data.product.finalPrice}
+            {formatCurrency(product.finalPrice)}
           </span>
           <span class="line-through text-2xl text-gray-600">
-            ${data.product.initialPrice}
+            {formatCurrency(product.initialPrice)}
           </span>
           <span
             class="text-xl font-bold bg-red-100 text-red-800 px-3 py-1 rounded-full"
           >
-            -{data.product.discountPercentage}%
+            -{product.discountPercentage}%
           </span>
         {:else}
           <span class="font-bold text-4xl text-gray-900">
-            ${data.product.finalPrice}
+            {formatCurrency(product.finalPrice)}
           </span>
         {/if}
       </div>
 
       <div class="flex gap-4 my-8">
-        <Button size="xl" class="px-8 py-3 text-lg font-semibold"
-          >Add to cart</Button
-        >
+        <Button size="xl" class="px-8 py-3 text-lg font-semibold">
+          Add to cart
+        </Button>
         <Button
           size="xl"
           color="alternative"
           class="px-8 py-3 text-lg font-semibold"
+          disabled
         >
           Add to wishlist
         </Button>
+        <Tooltip>To be implemented</Tooltip>
       </div>
 
       <div class="prose max-w-none">
         <h3 class="text-xl font-semibold mb-4">Product Details</h3>
         <p class="text-lg leading-relaxed text-gray-700">
-          {data.product.description}
+          {product.description}
         </p>
       </div>
     </div>
